ff49bba654c12b25c7f5888498d975c8fb6dc07b
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-07-11 12:41:38 +0530

    Add files via upload

---
+++ TechGig_Practice/OOPS/BatBowler.java
@@ -0,0 +1,30 @@
+/*Create two classes BATSMAN and BOWLER. Both the classes have same method name quality().
+The method quality() has the below text-
+CLASS BATSMAN
+quality() - I am BATSMAN
+CLASS BOWLER
+quality() - I am BOWLER too
+Make the object of class BOWLER.*/
+
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+   public static void main(String args[] ) throws Exception {
+      Batsman bat=new Batsman();
+      Bowler ball=new Bowler();
+      bat.quality();
+      ball.quality();
+	//Write code here
+
+   }
+}
+class Batsman{
+   public void quality(){
+      System.out.println("I am BATSMAN");
+   }
+}
+class Bowler{
+   public void quality(){
+      System.out.println("I am BOWLER too");
+   }
+}
