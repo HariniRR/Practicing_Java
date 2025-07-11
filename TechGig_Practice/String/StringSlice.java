bee6c2a3da1c52361f582fd49ec62b21329781cf
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-07-11 11:24:38 +0530

    Add files via upload

---
+++ TechGig_Practice/String/StringSlice.java
@@ -0,0 +1,28 @@
+/* You need to take string input and two other numbers which will be the start and end point of the slice and you need to print that slice of string.
+
+Input Format
+You will be given a function with string and two other integers as arguments.
+
+Constraints
+1 <= |S| <= 10^3
+
+Output Format
+You need to return the slice of the string.*/
+
+import java.io.*;
+import java.util.*;
+public class StringSlice {
+   public static void main(String args[] ) throws Exception {
+      Scanner s=new Scanner(System.in);
+      String inputstr=s.nextLine();
+      int st=s.nextInt();
+      int end=s.nextInt() +1;
+      System.out.print(inputstr.substring(st,end));
+	//Write code here
+
+   }
+}
+/* The inputString.substring(start, end) or getSlice(inputString, start, end) method in Java returns the part of the string from start (inclusive) to end (exclusive).
+
+
+
